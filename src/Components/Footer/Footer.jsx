7fbac2970cf713f6a5d9248f3c4331aa7ba97fcd
Prajapati Darshan Mahendrabhai <121242466+DarshanYT123@users.noreply.github.com>
2023-12-16 10:57:11 +0530

    UPDATED ASTRO 99.0

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 // import Map from "./Map";
 // import Map from "./Map";
+// import Map from "./Map";
 import FooterLinks from "./FooterLinks";
 
 const Footer = (props) => {
